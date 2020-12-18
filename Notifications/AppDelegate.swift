7095947c54ffa78f094e0be41c884a44033482ec
Dmitriy Danilin <danilindn@icloud.com>
2020-12-18 15:04:59 +0300

    Подготовка с созданием центра уведомлений

--- Notifications/AppDelegate.swift
+++ Notifications/AppDelegate.swift
@@ -7,40 +7,49 @@
 //
 
 import UIKit
+// Импортируем библиотеку уведомлений
+import UserNotifications
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    // Создаём экземпляр класса, для управлением уведомлениями. current возвращет обект для центра уведомлений
+    let notificationCenter = UNUserNotificationCenter.current()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Вызываем метод для запроса уведомлений
+        requestAutorisation()
+        
         return true
-    }
-
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-    }
-
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
-
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
     }
+    
 
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+    // Метод для запроса у пользователя разрешения на отправку уведомлений
+    func requestAutorisation() {
+        // Метод запроса авторизации. options это те уведомления которые мы хотим отправлять. Булево значение обозночает, прошла авторизация или нет
+        notificationCenter.requestAuthorization(options: [.alert, .sound, .badge]) { (granted, error) in
+            print("Разрешение получено: \(granted)")
+            
+            // Если авторизация проходит успешно, мы получаем перечень доступных уведомлений
+            guard granted else { return }
+            self.getNotificationsSetting()
+        }
     }
-
+    
+    // Метод для отслеживания настроек (включены или отключены уведомления)
+    func getNotificationsSetting() {
+        // Проверяем состояние авторизаций или параметров уведомлений
+        notificationCenter.getNotificationSettings { (settings) in
+            print("Настройки уведомлений: \(settings)")
+        }
+    }
 
 }
 
